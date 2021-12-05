71d9ea134d1403ac4347427b17b47bfd0193f326
Kaustubh BM <kaustubhbm3574@gmail.com>
2021-12-05 14:44:02 +0530

    Update Email_Spammer.py
    Added html parsing functionality

--- Email_Spammer.py
+++ Email_Spammer.py
@@ -1,21 +1,34 @@
 import smtplib, ssl, sys
 import getpass
 import argparse
+import email.message
 parser = argparse.ArgumentParser(description = "This is a program used to send Multiline Emails to single/multiple users. It can also be used as a Email Spammer.")
 parser.add_argument('-sid',metavar="sender-email-id",type=str,help='Enter the senders email ID.',dest='sender',required=True)
 parser.add_argument('-rid',metavar="--receiver-email-id",type=str,help="Enter a list of email-ids of the receivers separated by commas (,)",dest='list_of_receivers',required=True)
 parser.add_argument('-sn',metavar= "--spam-number",type=int,help="Enter the number of times the email must be sent to the receivers.",dest='number_of_messages',default=1)
 args = parser.parse_args()
 
-def Email_sender(sender,list_of_receivers,password,message,number_of_messages):
+def Email_sender(sender,list_of_receivers,password,message,subject,number_of_messages):
     if check(sender,list_of_receivers) == 1:
         print('Sending Mail')
         for i in range(number_of_messages):
             for receiver in list_of_receivers:
-                with smtplib.SMTP_SSL("smtp.gmail.com", port, context = ssl.create_default_context()) as server:
-                    server.login(sender, password)
-                    server.sendmail(sender,receiver,message)
-                    print('Mail Sent')
+                try:
+                    with smtplib.SMTP_SSL("smtp.gmail.com", port, context = ssl.create_default_context()) as server:
+
+                        msg = email.message.Message()
+                        msg['Subject'] = subject
+                        msg['From'] = sender
+                        msg['To'] = receiver
+                        msg.add_header('Content-Type','text/html')
+                        msg.set_payload(message)
+                        server.login(sender,password)
+                        server.sendmail(msg['From'], [msg['To']], msg.as_string())
+                        server.quit()
+                        print('Email sent.')
+                except:
+                    print("Login error...")
+                    exit(0)
     else:
         print("Incorrect email ID.")
         print("This program only supports the gmail smtp service.")
@@ -51,9 +64,10 @@ number_of_messages = args.number_of_messages
 sender = args.sender
 print("The sender's email address is " + str(sender))
 password = getpass.getpass()
-print('You are now logged in.')
+print("Please enter the subject of the email:")
+subject = input()
 print("Please enter the contents of your email in multiline format.")
 print("To end the mail please press ENTER twice.")
 message = entermessage()
-Email_sender(sender,list_of_receivers,password,message,number_of_messages)
-print('Ending Script')
+Email_sender(sender,list_of_receivers,password,message,subject,number_of_messages)
+print("Ending Script")
